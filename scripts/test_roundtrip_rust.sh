a50eb33cb5a381b9163bac14b7d593374cba29d2
Frey, Jed <dapperfu@users.github.com>
2025-06-13 06:34:57 -0400

    scripts/test_roundtrip_rust.sh: Fix command syntax to properly use backend flag with encode/decode subcommands

--- scripts/test_roundtrip_rust.sh
+++ scripts/test_roundtrip_rust.sh
@@ -24,9 +24,9 @@ run_test_backend() {
 
     # Encode
     if [ -n "$password" ]; then
-        venv/bin/whitespace-stego --backend "$backend" encode -mf "$msgfile" -cf "$carrierfile" -p "$password" -o "$encodedfile"
+        venv/bin/whitespace-stego encode --backend "$backend" -mf "$msgfile" -cf "$carrierfile" -p "$password" -o "$encodedfile"
     else
-        venv/bin/whitespace-stego --backend "$backend" encode -mf "$msgfile" -cf "$carrierfile" -o "$encodedfile"
+        venv/bin/whitespace-stego encode --backend "$backend" -mf "$msgfile" -cf "$carrierfile" -o "$encodedfile"
     fi
     if [ $? -ne 0 ]; then
         echo -e "${RED}    Encoding failed${NC}"
@@ -38,9 +38,9 @@ run_test_backend() {
 
     # Decode
     if [ -n "$password" ]; then
-        venv/bin/whitespace-stego --backend "$backend" decode -if "$encodedfile" -p "$password" -o "$decodedfile"
+        venv/bin/whitespace-stego decode --backend "$backend" -if "$encodedfile" -p "$password" -o "$decodedfile"
     else
-        venv/bin/whitespace-stego --backend "$backend" decode -if "$encodedfile" -o "$decodedfile"
+        venv/bin/whitespace-stego decode --backend "$backend" -if "$encodedfile" -o "$decodedfile"
     fi
     if [ $? -ne 0 ]; then
         echo -e "${RED}    Decoding failed${NC}"
